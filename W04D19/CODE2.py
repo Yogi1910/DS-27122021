4df750cd68ac07595330113a8458783efa3c7ddf
Yogi1910 <30946640+Yogi1910@users.noreply.github.com>
2022-01-27 21:48:09 +0530

    Add files via upload

---
+++ W04D19/CODE2.py
@@ -0,0 +1,15 @@
+def factorial(x):
+    
+    if x ==1:
+        return 1
+    else:
+        return (x*factorial(x-1))
+    
+    
+factorial(5)
+
+L1 = [1,2,3,400]
+x= L1.pop()
+
+print(L1,x)
+
---
+++ W04D19/PRINT_CHECK.py
@@ -0,0 +1,6 @@
+a = 2
+b= 10
+
+print(a*b)
+
+print('code has been excuted!')
